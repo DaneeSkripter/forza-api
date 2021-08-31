dd49c20f44aa12de0c8894a787815a4df9ff5266
DaneeSkripter <63862773+DaneeSkripter@users.noreply.github.com>
2021-08-31 13:19:10 +0200

    ✨ Update links

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const app = express()
       "https://images.forza-api.tk/BMW_M5_2018.png",
       "https://images.forza-api.tk/FORD_MUSTANG_GT_2018.png",
       "https://images.forza-api.tk/LAMBORGHINI_GALLARDO.png",
-      "https://images.forza-api.tk/TOYOTA_SUPRA_RZ_1998.png"
+      "https://images.forza-api.tk/TOYOTA_SUPRA_RZ_1998.png",
+      "https://images.forza-api.tk/AUDI_RS7_SPORTBACK.png",
+      "https://images.forza-api.tk/ASCARI_KZ1R_2012.png",
+      "https://images.forza-api.tk/FERRARI_FXX_K_2014.png",
+      "https://images.forza-api.tk/LAMBORGHINI_HURACÁN_LP_610-4_2014.png",
+      "https://images.forza-api.tk/JAGUAR_F-TYPE_R_COUPÉ_2015.png",
+      "https://images.forza-api.tk/MERCEDES-AMG_GT_R_2017.png",
     ]
     const randomimage = images[Math.floor(Math.random() * images.length)]
     res.header("Access-Control-Allow-Origin", "*")
